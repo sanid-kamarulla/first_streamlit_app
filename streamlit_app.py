29ae6a44bf016be00b35a753ec9bccc090ad021d
SANID KAMARULLA SAYEED <86715112+sanid-kamarulla@users.noreply.github.com>
2023-08-09 13:20:09 +0530

    Update streamlit_app.py
    added a breakfast item
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New halthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & ROcket Smoothie')
-streamlit.text('Hard Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & ROcket Smoothie')
+streamlit.text('🐔 Hard Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')

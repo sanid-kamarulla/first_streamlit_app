448cc93e7f9091227f551dc2591334e4c314b0fe
SANID KAMARULLA SAYEED <86715112+sanid-kamarulla@users.noreply.github.com>
2023-08-09 20:32:51 +0530

    Update streamlit_app.py
    added a text entry box to find the fruit choice.
    use this as a variable in the fruity vice API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,11 +22,14 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to call the Fruityvice API
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # normalise the json response to tabular format and save it to a variable
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # display the new normalised variable
 streamlit.dataframe(fruityvice_normalized)
+

9bf73dd7f9d6b7fb77e855552fc9a5f1d1cfff95
SANID KAMARULLA SAYEED <86715112+sanid-kamarulla@users.noreply.github.com>
2023-08-10 06:48:28 +0530

    Update streamlit_app.py
    previous one giving error. just checking back

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ import snowflake.connector
 #Query for trial account metadata
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from FRUIT_LOAD_LIST")
+my_cur.execute("current_role()")
 my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
+streamlit.text("The current role is:")
 streamlit.text(my_data_row)

6c6d08b349a1dc5a46232bb7cdbcc05199149d40
SANID KAMARULLA SAYEED <86715112+sanid-kamarulla@users.noreply.github.com>
2023-08-10 08:55:26 +0530

    Update streamlit_app.py
    added a function to insert new fruits to the fruit load list
    added a button to add a fruit to the list

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,12 +59,18 @@ if streamlit.button('Get the Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
+#Add a second text entry box for allowing the user adding a fruit
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('from streamlit')")
+    return "Thanks for adding " + new_fruit
+    
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
+
+
 #Don't run anything from here while we fix the control of flow
 streamlit.stop()
-
-#Add a second text entry box for adding fruit
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
-
-#start adding an insert command. The control of flow is not fixed.
-my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('from streamlit')");

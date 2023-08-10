c07a109ac37e6c5c6dc5c66af58ece85157aa6de
SANID KAMARULLA SAYEED <86715112+sanid-kamarulla@users.noreply.github.com>
2023-08-10 07:58:01 +0530

    Update streamlit_app.py
    covered the fruityvice API response in try except so that it is seperated from the rest of the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,18 +24,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-#New section to call the Fruityvice API
+#New section to call the Fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# normalise the json response to tabular format and save it to a variable
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# display the new normalised variable
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLEror as e:
+  streamlit.error()
 
 #Don't run anything from here while we fix the control of flow
 streamlit.stop()
